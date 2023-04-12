a4ba32c40b002bc8f3426d16d5eae140648da51c
dinhngocnam <nam.dn@qasoftsolution.com>
2023-04-12 18:14:21 +0700

    add API user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,11 +7,17 @@ import {
   Delete,
   UseGuards,
   ParseIntPipe,
+  Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UserEntity } from './entities/user.entity';
 
@@ -19,6 +25,14 @@ import { UserEntity } from './entities/user.entity';
 @ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  async create(@Body() createUserDto: CreateUserDto) {
+    return new UserEntity(await this.usersService.create(createUserDto));
+  }
 
   @Get()
   @UseGuards(JwtAuthGuard)
@@ -29,10 +43,16 @@ export class UsersController {
     return users.map((user) => new UserEntity(user));
   }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(+id, updateUserDto);
-  // }
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return new UserEntity(await this.usersService.update(id, updateUserDto));
+  }
 
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+export const roundsOfHashing = 10;
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.users.findUnique({
+      where: { email: createUserDto.email },
+    });
+    console.log(user);
 
+    if (user) {
+      throw new NotAcceptableException('email existed!');
+    }
+    const hashedPassword = await bcrypt.hash(
+      createUserDto.password,
+      roundsOfHashing,
+    );
+
+    createUserDto.password = hashedPassword;
+
+    return this.prisma.users.create({ data: createUserDto });
+  }
   async findAll() {
     const users = await this.prisma.users.findMany();
     return users;
   }
-
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.users.findUnique({ where: { id } });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(
+        updateUserDto.password,
+        roundsOfHashing,
+      );
+    }
+    return this.prisma.users.update({ where: { id }, data: updateUserDto });
   }
 
   async remove(id: number) {
